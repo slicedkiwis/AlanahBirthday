474f346bb38c4976f2c4504e1bf1da120191a609
Steeve Castor <steevecastor1@gmail.com>
2025-11-17 03:01:43 -0500

    Update birthday memory app with router basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/alanahsbirthday">
+    <Router basename="/alanahbirthday">
       <div className="App">
         <HeartAnimation />
         <Routes>
